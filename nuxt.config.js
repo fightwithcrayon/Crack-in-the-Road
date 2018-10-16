d816ff3d04ae17e1eb03b440cb43a4ef4b702d64
Andrew <>
2018-10-16 09:19:44 +0100

    Adjust generator

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'First to break the latest in genuinely groundbreaking new music, art and culture since 2010. Jacob Rees Mogg is our pay pig..' }
+      { hid: 'description', name: 'description', content: 'First to break the latest in genuinely groundbreaking new music, art and culture since 2010. Jacob Rees Mogg is our pay pig.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -56,30 +56,26 @@ module.exports = {
     }
   },
   generate: {
+    workers: 4,
+    workerConcurrency: 500,
+    concurrency: 500,
     routes: async function () {
       const res = await axios.get('https://admin.crackintheroad.com/wp-json/custom/routes/')
       const permalinks = res.data
-      //console.log('permalinks: ', permalinks)
-      console.log('size: ', Object.keys(permalinks).length)
       const pageSize = 1000
       const pageCount = Math.ceil(Object.keys(permalinks).length / pageSize)
-      console.log('pages: ', pageCount)
       let posts = []
       for (let pageNum = 1; pageNum <= pageCount; pageNum++) {
         await axios.get(`https://admin.crackintheroad.com/wp-json/custom/routes/${pageNum}`).then((res) => {
-          // console.log('retrieved page: ', pageNum)
           const formattedPosts = res.data.map((post, i) => {
-            // console.log((i + ((pageNum - 1) * pageSize) ))
             return {
               route: permalinks[post.ID].permalink,
               payload: post
             }
           })
           posts = [...posts, ...formattedPosts]
-          console.log('New size: ', posts.length)
         })
       }
-      // console.log('final post data generated:', posts)
       return posts
     }
   }
