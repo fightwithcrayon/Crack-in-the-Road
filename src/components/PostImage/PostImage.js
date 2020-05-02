a770d517887f4285db16f1da2b1e6b5f04881b35
Andrew Hill <andrew.hill@random.studio>
2020-05-02 22:36:30 +0200

    Better image loading

--- src/components/PostImage/PostImage.js
+++ src/components/PostImage/PostImage.js
@@ -1,39 +1,74 @@
 import styles from './PostImage.module.scss';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const PostImage = ({ alt, className, image, isLazy, sizes, srcset }) => {
+	const [isInViewport, setIsInViewport] = useState(false);
+	const imageRef = useRef();
 	const image_url = image ? `https://api.crackintheroad.com/images/${image}` : '';
+
+	useEffect(() => {
+		if ('loading' in new Image()) {
+			setIsInViewport(true);
+		}
+
+		if (isInViewport) {
+			return undefined;
+		}
+
+		const supportsObserver = ('IntersectionObserver' in window) ||
+			('IntersectionObserverEntry' in window) ||
+			('intersectionRatio' in window.IntersectionObserverEntry.prototype);
+
+		let ref = imageRef.current;
+		if (!supportsObserver) {
+			return undefined;
+		}
+
+		const handleIntersect = ([entry, ...rest]) => {
+			if (entry.isIntersecting) {
+				setIsInViewport(true);
+			}
+		}
+		const observer = new IntersectionObserver(handleIntersect);
+		observer.observe(ref);
+		return () => {
+			observer.disconnect();
+		}
+	}, [image_url, isInViewport]);
 
 	if (!image_url) {
 		return null;
 	}
 
 	return (
-		<div className={`${styles.image} ${className}`}>
-			<picture>
-				<source
-					sizes={sizes}
-					srcSet={srcset.map(size =>
-						image_url + `?width=${size}&type=webp ${size}w`
-					).join(', ')}
-					type="image/webp"
-				/>
-				<img
-					alt={alt}
-					loading={isLazy ? 'lazy' : ''}
-					sizes={sizes}
-					srcSet={srcset.map(size =>
-						image_url + `?width=${size} ${size}w`
-					).join(', ')}
-					src={image_url}
-				/>
-			</picture>
+		<div className={`${styles.image} ${isInViewport ? '' : styles.pending} ${className}`} ref={imageRef}>
+			{isInViewport && (
+				<picture>
+					<source
+						sizes={sizes}
+						srcSet={srcset.map(size =>
+							image_url + `?width=${size}&type=webp ${size}w`
+						).join(', ')}
+						type="image/webp"
+					/>
+					<img
+						alt={alt}
+						loading={isLazy ? 'lazy' : ''}
+						sizes={sizes}
+						srcSet={srcset.map(size =>
+							image_url + `?width=${size} ${size}w`
+						).join(', ')}
+						src={image_url}
+					/>
+				</picture>
+
+			)}
 		</div>
 	);
 }
 
 PostImage.defaultProps = {
-	className: null,
+	className: '',
 	isLazy: true,
 	ratio: 66,
 };
